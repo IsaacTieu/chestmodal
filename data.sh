06a6d22b9a236682c3c8df40bd3c2374b978b515
IsaacTieu <isaactieu@gmail.com>
2025-04-11 15:11:51 -0400

    fixed issue with Unix/Windows incompatability and /r

--- data.sh
+++ data.sh
@@ -1,30 +1,28 @@
-#!/bin/bash
-
 echo -n "Enter your PhysioNet password: "
 read -s PASSWORD
 echo "" 
 
-# Create a base directory for downloads
+# Base directory for downloads
 mkdir -p mimic_subset
 
 # Skip the header
 tail -n +2 sampled_500_studies.csv | while IFS=, read -r subject_id study_id dicom_id path path_report
 do
-  # Create full local paths within the mimic_subset directory
+  # Remove carriage returns (\r) from variables. The Windows .csv format was conflicting with the Unix environment. This cuts off the /r
+  path_report=$(echo "$path_report" | tr -d '\r')
+  path=$(echo "$path" | tr -d '\r')
+  
   local_dicom_path="mimic_subset/$path"
   local_report_path="mimic_subset/$path_report"
-  
-  # Create directories for the DICOM file
+
   mkdir -p "$(dirname "$local_dicom_path")"
-  
-  # Create directories for the report file
   mkdir -p "$(dirname "$local_report_path")"
   
   echo "Downloading $path"
-  wget -c --user isaactieu --password="$PASSWORD" "https://physionet.org/files/mimic-cxr/2.1.0/$path" -O "$local_dicom_path"
+  wget -N -c --user isaactieu --password="$PASSWORD" "https://physionet.org/files/mimic-cxr/2.1.0/$path" -O "$local_dicom_path"
   
   echo "Downloading $path_report"
-  wget -c --user isaactieu --password="$PASSWORD" "https://physionet.org/files/mimic-cxr/2.1.0/$path_report" -O "$local_report_path"
+  wget -N -c --user isaactieu --password="$PASSWORD" "https://physionet.org/files/mimic-cxr/2.1.0/$path_report" -O "$local_report_path"
 done
 
 echo "Download complete!"
